f67e4756063e90324967be908bad3d38a34c1d2b
TiaraOliveira <tiaragomes@yahoo.com.br>
2022-07-04 11:16:06 -0300

    userId

--- src/controllers/valuesController.js
+++ src/controllers/valuesController.js
@@ -15,6 +15,7 @@ export async function getSolds(req, res) {
 
 export async function Cashin(req, res) {
   const entry = req.body;
+  const session = res.locals.session;
   const dia = dayjs().format("DD-MM");
   const entrySchema = joi.object({
     soldin: joi.number().required(),
@@ -25,7 +26,7 @@ export async function Cashin(req, res) {
     return res.sendStatus(422);
   }
 
-  await db.collection('sold').insertOne({ ...entry, type: "increase", dia:dia });
+  await db.collection('sold').insertOne({ ...entry, type: "increase", dia:dia, userId: session.userId  });
   res.status(201).send('Entrada criada com sucesso');
 }
 
